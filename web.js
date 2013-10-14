56b6dd5be6a3a48c97fde153014f5ff73f363c6a
Dillon Eng <dillon@Dillons-MacBook-Pro-2.local>
2013-10-13 20:58:01 -0700

    changed app secret..

--- web.js
+++ web.js
@@ -1,15 +1,19 @@
 var fs = require('fs');
 var express = require('express');
-var passport = require('passport'), FacebookStrategy = require('passport-facebook').Strategy;
+var passport = require('passport'),FacebookStrategy = require('passport-facebook').Strategy;
 
 passport.use(new FacebookStrategy({
 	    clientID: 1427037610852975,
-		clientSecret: 5d2400c124782c3b4022504b1443b19c,
-		callbackURL: "auth/facebook/callback"	
-	},
+		clientSecret:4b99195332102f955d290bcf1c515c92,
+		callbackURL: '/auth/facebook/callback'
+		},
 	function(accessToken, refreshToken, profile, done) {
-	    done(null, user);
-	}));
+	    User.findOrCreate(..., function(err, user) {
+		    if (err) { return done(err); }
+		    done(null, user);
+		});
+	}
+	));
 
 var app = express.createServer(express.logger());
 app.get(/auth/facebook', passport.authenticate('facebook'));
